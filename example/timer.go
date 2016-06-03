2a15ac8a5d1954ec1677367eb4b488ab58c43c31
zxfonline <zxfonline@sina.com>
2016-06-03 18:41:21 +0800

    golang timer event
    golang timer event

---
+++ example/timer.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/zxfonline/taskexcutor"
+	"github.com/zxfonline/timer"
+)
+
+func randInt(min int32, max int32) int32 {
+	return min + rand.Int31n(max-min)
+}
+
+func main() {
+	SysTimer := timer.NewTimer("", nil)
+	//	for i := 0; i < 1; i++ {
+	//		//		go func() {
+	//		obj := &Obj{
+	//			name: fmt.Sprintf("name_%d", i+1),
+	//			t1:   T1{name: fmt.Sprintf("t1_%d", i+1)},
+	//			t2:   T2{name: fmt.Sprintf("t2_%d", i+1)},
+	//		}
+	//		ev := SysTimer.AddTimerEvent(taskexcutor.NewTaskService(obj.t1.Processing), fmt.Sprintf("name1_%d", i+1), 0, time.Duration((100+int(randInt(1, 50))*i)*1e6), 10-i, true)
+	//		obj.t1.event = ev
+	//		ev = SysTimer.AddTimerEvent(taskexcutor.NewTaskService(obj.t2.Processing), fmt.Sprintf("name2_%d", i+1), 0, time.Duration((100+20*i)*1e6), 10-i, true)
+	//		obj.t2.event = ev
+	//		//		}()
+	//	}
+	//	time.Sleep(5 * time.Second)
+	t3 := &T3{name: "t3_1"}
+	//	ev3 := SysTimer.AddTimerEvent(taskexcutor.NewTaskService(t3.Processing), "name3_1", 0, time.Duration((100)*1e6), 3, true)
+	ev3 := SysTimer.AddTimerEvent(taskexcutor.NewTaskService(func(params ...interface{}) {
+		event := (params[0])
+		ev := (params[0]).(*timer.TimerEvent)
+		if ev.Closed() {
+			fmt.Println("closed")
+			return
+		}
+		fmt.Printf("event3=%p,%+v\n", event, event)
+		time.Sleep(1 * time.Second)
+		ev.Close()
+	}), "name3_1", 0, time.Duration((100)*1e6), 3, true)
+
+	t3.event = ev3
+	time.Sleep(5 * time.Second)
+}
+
+type Obj struct {
+	name string
+	t1   T1
+	t2   T2
+}
+type T1 struct {
+	name  string
+	event *timer.TimerEvent
+}
+
+func (t *T1) Processing(params ...interface{}) {
+	//	time.Sleep(2 * time.Second)
+	fmt.Printf("1=%v ,e=%p,name=%p\n", t, t, &t.name)
+}
+
+type T2 struct {
+	name  string
+	event *timer.TimerEvent
+}
+
+func (t T2) Processing(params ...interface{}) {
+	//	time.Sleep(1 * time.Second)
+	lm := t.name
+	fmt.Printf("2=%v ,e=%p,name=%s,lm=%s\n", t, t, t.name, lm)
+}
+
+type T3 struct {
+	name  string
+	event *timer.TimerEvent
+}
+
+func (t *T3) Processing(params ...interface{}) {
+	//	time.Sleep(1 * time.Second)
+
+	//	event := (params[0])
+	//	fmt.Printf("event0=%p,%+v\n", event, event)
+	//	ev := event.(*timer.TimerEvent)
+	//	fmt.Printf("event1=%p,%+v\n", ev, ev)
+	//	fmt.Printf("event2=%p,%+v\n", t.event, t.event)
+	//	fmt.Println(t.event == ev)
+	//	t.name = fmt.Sprintf("%d", randInt(1, 100))
+	fmt.Printf("3=%v ,e=%p,name=%p\n", t, t, &t.name)
+}
---
+++ timer.go
@@ -0,0 +1,212 @@
+// Copyright 2016 zxfonline@sina.com. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package timer
+
+import (
+	"container/heap"
+	"sync"
+	"time"
+
+	"github.com/zxfonline/golog"
+	"github.com/zxfonline/taskexcutor"
+)
+
+func Nanoseconds() int64 {
+	return time.Now().UnixNano()
+}
+
+const forever = 1 << 62
+
+const maxSleepTime = 1e9
+
+type Timer struct {
+	Logger         *golog.Logger
+	events         eventHeap
+	excutor        taskexcutor.Excutor
+	eventMutex     sync.Mutex
+	currentSleeper int64
+}
+
+//var SysTimer = NewTimer("",nil)
+
+func NewTimer(name string, excutor taskexcutor.Excutor) *Timer {
+	if name == "" {
+		name = "TimerExcutor"
+	}
+	logger := golog.New(name)
+	events := make([]*TimerEvent, 0, 128)
+	events = append(events, &TimerEvent{nextTime: forever})
+	if excutor == nil {
+		excutor = taskexcutor.NewTaskPoolExcutor(logger, 1, 65536, true, 0)
+	}
+	t := &Timer{Logger: logger, events: events, excutor: excutor}
+	heap.Init(&t.events)
+	return t
+}
+
+type TimerEvent struct {
+	i         int    //所属排序队列的index
+	Name      string //事件名称
+	count     int    //执行次数,当为0表示永久执行
+	costCount int    //已执行次数
+	absolute  bool   //决对时间定时，线程工作的时间计算在定时时间内
+	interval  int64  //定时执行时间
+	nextTime  int64  //上次执行时间
+	t         *Timer //所属定时器
+	//事件执行方法
+	listener *taskexcutor.TaskService
+}
+
+func (e *TimerEvent) Closed() bool {
+	return e.i < 0
+}
+func (e *TimerEvent) Close() {
+	if e.i < 0 {
+		return
+	}
+	e.t.eventMutex.Lock()
+	if e.i >= 0 {
+		heap.Remove(&e.t.events, e.i)
+	}
+	e.t.eventMutex.Unlock()
+}
+
+//添加定时事件，事件的回调函数默认第一个参数为 *TimerEvent
+func (t *Timer) AddOnceEvent(listener *taskexcutor.TaskService, name string, wait time.Duration) (e *TimerEvent) {
+	return t.AddTimerEvent(listener, name, wait, wait, 1, true)
+}
+
+//添加定时事件，事件的回调函数默认第一个参数为 *TimerEvent
+func (t *Timer) AddTimerEvent(listener *taskexcutor.TaskService, name string, initTime, interval time.Duration, count int, absolute bool) (e *TimerEvent) {
+	now := Nanoseconds()
+	init := initTime.Nanoseconds()
+	if init < 0 {
+		init = interval.Nanoseconds()
+	}
+	nextTime := now + init
+	if nextTime < now {
+		panic("timer: invalid timer start time")
+	}
+	if count < 0 {
+		count = 0
+	}
+	if interval.Nanoseconds() <= 0 && count == 0 {
+		panic("timer: invalid timer interval time")
+	}
+	t.eventMutex.Lock()
+	t0 := t.events[0].nextTime
+	e = &TimerEvent{
+		nextTime: nextTime,
+		interval: interval.Nanoseconds(),
+		count:    count,
+		absolute: absolute,
+		listener: listener,
+		t:        t,
+		Name:     name,
+		i:        -1,
+	}
+	//默认事件添加到第一个参数
+	if listener != nil {
+		listener.AddArgs(0, e)
+	}
+	heap.Push(&t.events, e)
+	if t0 > e.nextTime && (t0 == forever || init < maxSleepTime) {
+		t.currentSleeper++
+		go t.sleeper(t.currentSleeper)
+	}
+	t.eventMutex.Unlock()
+	return
+}
+
+func (t *Timer) sleeper(sleeperId int64) {
+	t.eventMutex.Lock()
+	e := t.events[0]
+	tm := Nanoseconds()
+	for e.nextTime != forever {
+		if dt := e.nextTime - tm; dt > 0 {
+			if dt > maxSleepTime {
+				dt = maxSleepTime
+			}
+			t.eventMutex.Unlock()
+			time.Sleep(time.Duration(dt))
+			t.eventMutex.Lock()
+			if t.currentSleeper != sleeperId {
+				break
+			}
+		}
+		e = t.events[0]
+		tm = Nanoseconds()
+		for tm >= e.nextTime {
+			if e.listener != nil {
+				if tm-e.nextTime >= 2*e.interval {
+					t.Logger.Warnf("interval run timeout,event=%+v,current=%v", e, tm)
+				}
+				if e.absolute {
+					e.nextTime += e.interval
+				} else {
+					e.nextTime = tm + e.interval
+				}
+				err := e.t.excutor.Excute(e.listener)
+				if e.count != 0 {
+					e.costCount++
+					if err != nil {
+						t.Logger.Warnf("interval run err=%v,event=%+v", err, e)
+						//					} else {
+						//						e.costCount++
+					}
+					if e.costCount >= e.count {
+						heap.Pop(&t.events)
+						e = t.events[0]
+					} else {
+						heap.Fix(&t.events, e.i)
+						e = t.events[0]
+					}
+				} else {
+					heap.Fix(&t.events, e.i)
+					e = t.events[0]
+				}
+			} else {
+				t.Logger.Warnf("run nil listener,event=%+v", e)
+				heap.Pop(&t.events)
+				e = t.events[0]
+			}
+		}
+	}
+	t.eventMutex.Unlock()
+}
+
+type eventHeap []*TimerEvent
+
+func (t *eventHeap) Len() int {
+	return len(*t)
+}
+
+func (t *eventHeap) Less(i, j int) bool {
+	return (*t)[i].nextTime < (*t)[j].nextTime
+}
+
+func (t *eventHeap) Swap(i, j int) {
+	tp := *t
+	tp[i], tp[j] = tp[j], tp[i]
+	tp[i].i = i
+	tp[j].i = j
+}
+
+func (t *eventHeap) Push(x interface{}) {
+	e := x.(*TimerEvent)
+	e.i = len(*t)
+	*t = append(*t, e)
+}
+
+func (t *eventHeap) Pop() interface{} {
+	old := *t
+	n := len(old) - 1
+	e := old[n]
+	*t = old[0:n]
+	e.i = -1
+	e.t.Logger.Debugf("closed:event=%+v.", e)
+	e.listener = nil
+	e.t = nil
+	return e
+}
